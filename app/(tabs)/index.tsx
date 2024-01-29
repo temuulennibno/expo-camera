0768304532d82edb8939dfc9036ef366d6f8ffc3
Temuulen Narmandakh <temuulen.n@pinecone.mn>
2024-01-29 17:17:08 +0800

    first commit

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,14 +1,47 @@
-import { StyleSheet } from 'react-native';
-
-import EditScreenInfo from '@/components/EditScreenInfo';
-import { Text, View } from '@/components/Themed';
+import { Button, Image, StyleSheet, Text, View } from "react-native";
+import * as ImagePicker from "expo-image-picker";
+import { useEffect, useState } from "react";
 
 export default function TabOneScreen() {
+  const [image, setImage] = useState("");
+  const [status, requestPermission] = ImagePicker.useCameraPermissions();
+
+  if (!status || !status.granted) {
+    return (
+      <View style={styles.container}>
+        <Text>Give camera permission</Text>
+        <Button title="Enable camera" onPress={requestPermission} />
+      </View>
+    );
+  }
+
+  const pickImage = async () => {
+    let result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.All,
+      allowsEditing: true,
+      aspect: [4, 3],
+      quality: 1,
+    });
+
+    if (!result.canceled) {
+      setImage(result.assets[0].uri);
+    }
+  };
+
+  const captureImage = async () => {
+    let result = await ImagePicker.launchCameraAsync({
+      quality: 1,
+    });
+    if (!result.canceled) {
+      setImage(result.assets[0].uri);
+    }
+  };
+
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Tab One</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="app/(tabs)/index.tsx" />
+      <Button title="Choose image from gallery" onPress={pickImage} />
+      <Button title="Capture image" onPress={captureImage} />
+      {image && <Image source={{ uri: image }} style={{ width: 200, height: 200 }} />}
     </View>
   );
 }
@@ -16,16 +49,16 @@ export default function TabOneScreen() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: "center",
+    justifyContent: "center",
   },
   title: {
     fontSize: 20,
-    fontWeight: 'bold',
+    fontWeight: "bold",
   },
   separator: {
     marginVertical: 30,
     height: 1,
-    width: '80%',
+    width: "80%",
   },
 });
--- app/(tabs)/two.tsx
+++ app/(tabs)/two.tsx
@@ -1,14 +1,41 @@
-import { StyleSheet } from 'react-native';
-
-import EditScreenInfo from '@/components/EditScreenInfo';
-import { Text, View } from '@/components/Themed';
+import { Camera, CameraType } from "expo-camera";
+import { useRef, useState } from "react";
+import { Button, Image, StyleSheet, Text, View } from "react-native";
 
 export default function TabTwoScreen() {
+  const [type, setType] = useState(CameraType.back);
+  const [image, setImage] = useState("");
+
+  const [camera, setCamera] = useState<any>(null);
+
+  const [status, requestPermission] = Camera.useCameraPermissions();
+
+  if (!status || !status.granted) {
+    return (
+      <View style={styles.container}>
+        <Text>Give camera permission</Text>
+        <Button title="Enable camera" onPress={requestPermission} />
+      </View>
+    );
+  }
+
+  const handleFlipCamera = () => {
+    setType(type === CameraType.back ? CameraType.front : CameraType.back);
+  };
+
+  const handleCapture = async () => {
+    if (camera) {
+      const result = await camera.takePictureAsync();
+      setImage(result.uri);
+    }
+  };
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Tab Two</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="app/(tabs)/two.tsx" />
+      <Camera style={{ flex: 1, width: "100%" }} type={type} ref={(ref) => setCamera(ref)}>
+        {image && <Image source={{ uri: image }} width={100} height={100} style={{ borderWidth: 1, borderColor: "red" }} />}
+        <Button title="Flip camera" onPress={handleFlipCamera} />
+        <Button title="Capture" onPress={handleCapture} />
+      </Camera>
     </View>
   );
 }
@@ -16,16 +43,16 @@ export default function TabTwoScreen() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: "center",
+    justifyContent: "center",
   },
   title: {
     fontSize: 20,
-    fontWeight: 'bold',
+    fontWeight: "bold",
   },
   separator: {
     marginVertical: 30,
     height: 1,
-    width: '80%',
+    width: "80%",
   },
 });
